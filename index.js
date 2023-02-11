6dac5e692779ca3a89b18b92432bb54ca53e57de
Jason <hylee.jason@gmail.com>
2023-02-12 04:45:14 +0800

    Add new code

--- index.js
+++ index.js
@@ -1,22 +1,24 @@
+module.exports = Phrase;
+
 // Adds `reverse` to all strings.
-String.prototype.reverse = function() {
+String.prototype.reverse = function () {
     return Array.from(this).reverse().join("");
 }
 
-// Defines a phrase object
 function Phrase(content) {
     this.content = content;
 
-    // Returns content processed for palindrome testing.
+    // Return content processed for palindrome testing.
     this.processedContent = function processedContent() {
-        return this.content.toLowerCase();
+        return this.content.match(/[a-z]/gi).join("").toLowerCase();
     }
 
     // Returns true if the phrase is a palindrome, false otherwise.
     this.palindrome = function palindrome() {
-        return this.processedContent() === this.processedContent().reverse();
+        if (this.content) {
+            return this.processedContent() === this.processedContent().reverse();
+        } else {
+            return false;
+        }
     }
 }
-
-let napoleonsLament = new Phrase("Able was I ere I saw Elba");
-console.log(napoleonsLament.palindrome())
